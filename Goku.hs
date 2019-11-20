9f3927d1483e80d7dee92fca340867bd3e0d2aa7
Hilderlan <hilakt9698@gmail.com>
2019-11-20 02:22:50 -0300

    Adicoes necessarias

--- Goku.hs
+++ Goku.hs
@@ -102,6 +102,7 @@ module ArvBusBin(ArvBusBin) where
     -- Fila segunda implementação (Resposta da questão)
   -- Definições das funções
   module Queue (Queue, enqueue, dequeue, front, queueEmpty, newQueue) where
+  -- data Queue t = Fila [t] [t] deriving (Show)
   enqueue :: t -> Queue t -> Queue t
   dequeue :: Queue t -> Queue t
   front :: Queue t -> t
@@ -111,14 +112,18 @@ module ArvBusBin(ArvBusBin) where
 
   -- Implementação otimizada
   newQueue = Fila [ ] [ ]
+
   queueEmpty (Fila [ ] [ ]) = True
   queueEmpty _ = False
+
   front (Fila [ ] [ ]) = error "A fila esta vazia"
   front (Fila (x:xs) _) = x
   front (Fila [ ] ys) = front (Fila (reverse ys) [ ])
+
   enqueue y (Fila [ ] [ ]) = (Fila [y] [ ])
   enqueue y (Fila [ ] ys) = (Fila [ ] (y : ys))
   enqueue y (Fila (x : xs) ys) = (Fila (x : xs) (y : ys))
+
   dequeue (Fila (x : xs) ys) = Fila xs ys
   dequeue (Fila [ ] [ ]) = error "A fila esta vazia"
   dequeue (Fila [ ] ys) = (Fila (tail (reverse ys)) [ ])
@@ -261,3 +266,195 @@ module ArvBusBin(ArvBusBin) where
   calculaPerimetro (Circulo raio) = 2 * pi * raio
   calculaPerimetro (Triangulo a1 a2 a3) = a1 + a2 + a3
   calculaPerimetro (Retangulo a1 a2) = 2*(a1 + a2)
+
+-- Anotações
+
+    -- Eficiência dos progranas funcionais
+  {-
+    Por default, Haskell usa lazy evaluation, mas também pode usar eager
+    evaluation, conhecida como avaliação gulosa, equivalente a passagem
+    de parâmetros por valor, o que torna os programas bastante rápidos e
+    eficientes.
+    • No entanto, a avaliação estrita pode não atingir a forma normal,
+    mesmo que ela exista, enquanto a avaliação preguiçosa garante a sua
+    atingibilidade. Isto é garantido pelo Teorema de Church-Rosser.
+    • No entanto, a avaliação preguiçosa pode exigir mais passos de
+    execução que a avaliação estrita.
+    • Alguns compiladores realizam uma verificação da adequabilidade, ou
+    não, da avaliação estrita, conhecida como strictness anylising.
+    • A análise de desempenho pode dar resultados diferentes se for
+    utilizada uma avaliação estrita ou uma avaliação preguiçosa
+  -}
+
+    -- Pilha primeira implementação
+  module Stack(Stack, push, pop, top, stackEmpty, newStack) where
+  push :: t -> Stack t -> Stack t
+  pop :: Stack t -> Stack t
+  top :: Stack t -> t
+  stackEmpty :: Stack t -> Bool
+  newStack :: Stack t
+  data Stack t = EmptyStk | Stk t (Stack t)
+
+  import Stack
+  listaParaPilha :: [t] -> Stack t
+  listaParaPilha [ ] = newStack
+  listaParaPilha (x : xs) = push x (listaParaPilha xs)
+  pilhaParaLista :: Stack t -> [t]
+  pilhaParaLista s
+  |stackEmpty s = [ ]
+  |otherwise = (top s) : (pilhaParaLista (pop s))
+
+  top EmptyStk = error “topo de pilha vazia”
+  top (Stk x _) = x
+
+  newStack = EmptyStk
+
+  stackEmpty EmptyStk = True
+  stackEmpty _ = False
+
+  instance (Show t) => Show (Stack t) where
+  show (EmptyStk) = “#”
+  show (Stk x s) = (show x) ++ “|” ++ (show s)
+
+  push x s = Stk x s
+
+  pop EmptyStk = error “retirada de Pilha vazia”
+  pop (Stk _ s) = s
+
+    -- Pilha segunda implementação
+    module Stack(Stack, push, pop, top, stackEmpty, newStack} where
+      push :: t -> Stack t -> Stack t
+      pop :: Stack t -> Stack t
+      top :: Stack t -> t
+      stackEmpty :: Stack t -> Bool
+      newStack :: Stack t
+      data Stack t = Stk [t]
+  
+    push x (Stk xs) = Stk (x : xs)
+
+    pop (Stk [ ]) = error "retirada em pilha vazia"
+    pop (Stk (_ : xs)) = Stk xs
+
+    top (Stk [ ]) = error "topo de pilha vazia"
+    top (Stk (x : _)) = x
+
+    newStack = Stk [ ]
+
+    stackEmpty (Stk [ ]) = True
+    stackEmpty _ = False
+
+    instance (Show t) => Show (Stack t) where
+
+    show (Stk [ ]) = "#"
+    show (Stk (x : xs)) = (show x) ++ "|" ++ (show (Stk xs))
+
+    -- Tipo abstrato Set
+  module Set (Set, emptySet, setEmpty, inSet, addSet, delSet) where
+    emptySet :: Set t
+    setEmpty :: Set t -> Bool
+    inSet :: (Eq t) => t -> Set t -> Bool
+    addSet :: (Eq t) => t -> Set t -> Set t
+    delSet :: (Eq t) => t -> Set t -> Set t
+    pickSet :: Set t -> t
+    data Set t = S [t] --listas sem repetições
+
+  emptySet = S [ ]
+
+  setEmpty (S [ ]) = True
+  setEmpty _ = False
+
+  inSet _ (S [ ]) = False
+  inSet x (S (y : ys))
+    |x == y = True
+    |otherwise = inSet x (S ys)
+
+  addSet x (S s)
+    |(elem x s) = S s
+    |otherwise = S (x : s)
+
+  delSet x (S s) = S (delete x s)
+  delete x [ ] = [ ]
+  delete x (y : ys)
+    |x == y = delete x ys
+    |otherwise = y : (delete x ys)
+
+  pickSet (S [ ]) = error "conjunto vazio"
+  pickSet (S (x : _)) = x
+
+    -- Tipo abstrato Table
+  module Table (Table, newTable, findTable, updateTable, removeTable) where
+    newTable :: Table a b
+    findTable :: (Ord a) => a -> Table a b -> Maybe b
+    updateTable :: (Ord a) => (a, b) -> Table a b -> Table a b
+    removeTable :: (Ord a) => a -> Table a b -> Table a b
+    data Table a b = Tab [(a,b)] --lista ordenada de forma crescente
+
+  newTable = Tab [ ]
+
+  findTable _ (Tab [ ]) = Nothing
+  findTable x (Tab ((c,v) : resto))
+    |x < c = Nothing
+    |x == c = Just v
+    |x > c = findTable x (Tab resto)
+
+  updateTable (x, z) (Tab [ ]) = Tab [(x, z)]
+  updateTable (x, z) (Tab ((c,v) : resto))
+    |x < c = Tab ((x,z):(c,v):resto)
+    |x == c = Tab ((c,z):resto)
+    |x > c = let (Tab t) = updateTable (x,z) (Tab resto) in Tab ((c,v):t)
+
+  removeTable _ (Tab [ ]) = Tab [ ]
+  removeTable x (Tab ((c,v):resto))
+    |x < c = Tab ((c,v):resto)
+    |x == c = Tab resto
+    |x > c = let (Tab t) = removeTable x (Tab resto) in Tab ((c,v):t)
+
+  instance (Show a, Show b) => Show (Table a b) where
+    show (Tab [ ]) = " "
+    show (Tab ((c,v):resto))
+    = (show c)++"\t"++(show v)++"\n"++(show (Tab resto))
+
+
+    -- Ações ----------------------------------------------------
+{-
+  É um tipo especial de dado em Haskell. Quando o sistema avalia uma expressão cujo
+  resultado seja uma ação, ele sabe que não deve mostrar este resultado no dispositivo
+  padrão de saída e sim, apenas executar a ação apropriada.
+  • Ações primitivas
+  Escrever um caractere em um arquivo ou recebê-lo via teclado.
+  • Ações compostas
+  Imprimir uma String inteira em um arquivo
+  • Comandos
+  Funções em Haskell, cujos resultados de suas avaliações sejam ações
+-}
+
+  faz4vezes :: String -> IO ( )
+  faz4vezes str = do  putStrLn str
+                      putStrLn str
+                      putStrLn str
+                      putStrLn str
+
+  fazNvezes :: Int -> String -> IO ( )
+  fazNvezes n str = if n <= 1
+                      then putStrLn str
+                      else do
+                        putStrLn str
+                        fazNvezes (n-1) str
+                        e faz faz4vezes = fazNvezes 4
+
+  -- Arquivos
+  import System.IO
+  import Data.Char (toUpper)
+  main :: IO ( )
+  main = do entrada <- openFile “entra.txt” ReadMode
+  saída <- openFile “sai.txt” WriteMode
+  loop_principal entrada saida
+  hClose entrada
+  hClose saída
+  loop_principal :: Handle -> Handle -> IO ( )
+  loop_principal entrada saída =
+  do fim_de_arquivo <-hIsEOF entrada
+  if fim_de_arquivo then return ( )
+  else do inpStr <- hGetLine entrada
+  hPutStrLn saída (map toUpper inpStr)
+  loop_principal entrada saida
